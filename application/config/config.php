9f3bbd596b37c600c7f70114741dea60de0eaf3b
marcelo <marcelo170690@gmail.com>
2020-03-02 15:07:21 -0400

    cambio de docx

--- application/config/config.php
+++ application/config/config.php
@@ -24,8 +24,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-//$config['base_url'] = 'http://localhost:8080/belen';
- $config['base_url'] = 'http://localhost/belen';
+$config['base_url'] = 'http://localhost:8080/belen';
+//  $config['base_url'] = 'http://localhost/belen';
 //$config['base_url'] = 'http://192.168.1.100/spa';
 
 /*
